6f01892f02360169c6766255a9f9b91daf2564ac
Lreed03 <lreed0383@gmail.com>
2025-07-16 11:03:50 -0600

    Implement task addition functionality in Todo component

--- frontend/src/pages/Todo.js
+++ frontend/src/pages/Todo.js
@@ -1,10 +1,25 @@
-import React from "react";
+import { useState } from "react";
 
 function Todo() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  function addTask() {
+    if (input.trim() === "") {
+      return; 
+    }
+    setTasks([...tasks, input]);
+    setInput(""); 
+  }
+
+  const listTasks = tasks.map((task, index) => (
+    <li key={index}>{task}</li>
+  ));
   return (
     <div>
-      <input type="text" placeholder="Add a new task" />
-      <button>Add Task</button>
+      <input type="text" placeholder="Add a new task" value={input} onChange={(e) => setInput(e.target.value)}/>
+      <button onClick={addTask}>Add Task</button>
+      <ol>{listTasks}</ol>
     </div>
   );
 }
