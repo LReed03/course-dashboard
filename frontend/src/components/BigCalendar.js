98765a7879b7cb0765fc5acd32d2559c078ed2ef
Lreed03 <lreed0383@gmail.com>
2025-08-04 15:44:03 -0600

    Fixed some styling of the calendar

--- frontend/src/components/BigCalendar.js
+++ frontend/src/components/BigCalendar.js
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Calendar, CalendarProps, momentLocalizer } from 'react-big-calendar';
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { loadCourses } from "../api/courseAPI";
+import { loadTasks} from "../api/TodoAPI";
 
 function BigCalendar(props){
     const localizer = momentLocalizer(moment);
+    const [tasks, setTasks] = useState([]);
+    const [courses, setCourses] = useState([]);
+    
+
+    async function fetchData() {
+        let taskList = await loadTasks();
+        let courseList = await loadCourses();
+        setTasks(taskList);                 
+        setCourses(courseList);
+        }
+    useEffect(() => {
+        fetchData();
+        }, []);
 
     return(
         <div style={{ height: "90vh"}}>
