cbdda8eac756743b0f14729a97e7fbf2018393f3
Lreed03 <lreed0383@gmail.com>
2025-08-13 17:11:28 -0600

    Fixed buttons on the calendar

--- frontend/src/components/CalendarComponent.js
+++ frontend/src/components/CalendarComponent.js
@@ -1,29 +1,27 @@
-import React, { useState, useEffect } from "react";
-import { Calendar, CalendarProps, momentLocalizer } from 'react-big-calendar';
+import React, { useEffect, useState } from "react";
+import { Calendar as RBCalendar, Views, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
-import { loadCourses } from "../api/courseAPI";
-import { loadTasks} from "../api/TodoAPI";
 
-function CalendarComponent(props){
-    const localizer = momentLocalizer(moment);
-    const [tasks, setTasks] = useState([]);
-    const [courses, setCourses] = useState([]);
-    
+const localizer = momentLocalizer(moment);
 
-    async function fetchData() {
-        let taskList = await loadTasks();
-        let courseList = await loadCourses();
-        setTasks(taskList);                 
-        setCourses(courseList);
-        }
-    useEffect(() => {
-        fetchData();
-        }, []);
+export default function CalendarComponent() {
+  const [events] = useState([]);
+  const [view, setView] = useState(Views.MONTH);
 
-    return(
-        <Calendar localizer={localizer} events={[]} style={{ height: "90vh"}}/>
-    );
+  return (
+    <div style={{ height: "90vh" }}>
+      <RBCalendar
+        localizer={localizer}
+        events={events}
+        view={view}
+        onView={(v) => {
+          console.log("onView fired ->", v);
+          setView(v);
+        }}
+        defaultView={Views.MONTH}
+        views={[Views.MONTH, Views.WEEK, Views.DAY, Views.AGENDA]}
+      />
+    </div>
+  );
 }
-
-export default CalendarComponent;
