c05705263cdd4194062810c8628bdd458dac8bc2
lreed03 <lreed0383@gmail.com>
2025-08-18 22:43:43 -0600

    Changed header to keep track of log in

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Header.css";
+import { useAuth } from "../contexts/authcontext";
 
 function Header() {
+  const {userLoggedIn} = useAuth();
   return (
     <header className="header">
       <div className="logo">
         <Link to="/">CourseTrack</Link>
       </div>
+      {!userLoggedIn ? 
+      <nav className="nav-links">
+        <Link to="/login">Login</Link> 
+        <Link to="/signup">Signup</Link>
+      </nav>
+        :
       <nav className="nav-links">
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/todo">To-Do</Link>
         <Link to="/schedule">Schedule</Link>
-      </nav>
+      </nav>}
     </header>
   );
 }
