0d0c590732c236a77b17a482aa3a012eaf159916
Lreed03 <lreed0383@gmail.com>
2025-09-09 13:43:32 -0600

    Changed some confirm delete and such

--- frontend/src/components/ConfirmDelete.js
+++ frontend/src/components/ConfirmDelete.js
@@ -1,8 +1,12 @@
 import React from "react";
+import "../styles/ConfirmDelete.css";
 
 function ConfirmDelete(props){
     return(
-        <div></div>
+        <div className="confirm-delete-container">
+            <p>Are you sure you want to remove this class?</p>
+            <button onClick={props.ConfirmDelete}>Confirm</button>
+        </div>
     )
 }
 
